a7f81f1a0841f4cca85ba79953d43af6463f71ee
mfgmk <mike@epictum.com>
2015-10-30 17:00:18 -0700

    adding wp hhvm site

---
+++ wp-salts.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         '^PuH]@zn|@Rv>vb)H)ar`jN$edg;d(I}dq4,QWD{VVr$|+u.4]#-4R(w@a3m3j4l');
+define('SECURE_AUTH_KEY',  'n(h-_Z$~][$IH#:I>A[)%ZW17ijZ38Y-4PW}bq%-(<&E{o%)[WrD~T%C*OM(`MFb');
+define('LOGGED_IN_KEY',    '+@|1e|:f>8D-f(F1gdy7ox{;F$3klk1wztNS>Sy3=~D?5Ev0bPPiJxIr;n_8w=0*');
+define('NONCE_KEY',        ';i]1+~-^-rYgN[cMXz/i}:oQG>hLKvu~8LD2OZR,K6o-M&^P*Xjh>@|<OGN!A=!^');
+define('AUTH_SALT',        'e).*v?[Rb[X7 8>++Mco.s|XZ+!iRZ_GIvsVK&DiT>f2IG+N5!_E[(U/.{JG`Vf ');
+define('SECURE_AUTH_SALT', '8lp]{u)e+|P$CZVc.+B.4Y2Csj}P]YsDEEh4l$(K1l/zc-;+{Gm;]- (v8 &/ibp');
+define('LOGGED_IN_SALT',   '@$u4I:nXI[M%VO>x EJ5(M;dV<XX+/6YmvAb.cj<;}{ff(=?+W*y3kj-o7Pm<vk8');
+define('NONCE_SALT',       'lj+XTW>)G=n8KiQ.:00AgbG8j*.+~Q/LETKSniYWt|peMCnEtk@?WYWX3`CU?US)');
